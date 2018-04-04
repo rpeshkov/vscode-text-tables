caf10b10ca6caa787412bf07cab46953f8906184
Roman Peshkov <peshkovroman@gmail.com>
2018-04-05 01:06:14 +0200

    Main extension file refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 
 import * as vscode from 'vscode';
 import { OrgLocator, OrgParser, OrgStringifier  } from './ttOrg';
-import { Locator, Parser, Stringifier, TableNavigator } from './ttTable';
+import { Locator, Parser, Stringifier, TableNavigator, Table } from './ttTable';
 import { MarkdownLocator, MarkdownParser, MarkdownStringifier } from './ttMarkdown';
 import { isUndefined } from 'util';
 
@@ -63,63 +63,60 @@ export function activate(ctx: vscode.ExtensionContext) {
         }));
 
     ctx.subscriptions.push(vscode.commands.registerCommand('text-tables.gotoNextCell', () => {
-        if (!isUndefined(vscode.window.activeTextEditor)) {
-            const editor = vscode.window.activeTextEditor;
-            const tableRange = locator.locate(editor.document, editor.selection.start.line);
-
-            if (tableRange !== undefined) {
-                const tableText = editor.document.getText(tableRange);
-                const table = parser.parse(tableText);
-
-                if (table !== undefined) {
-                    const newText = stringifier.stringify(table);
-                    editor.edit(b => b.replace(tableRange, newText));
-                    const nav = new TableNavigator(table);
-                    const newPos = nav.nextCell(editor.selection.start);
-                    editor.selection = new vscode.Selection(newPos, newPos);
-                }
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const table = formatAndGetTableUnderCursor(editor);
+            if (table) {
+                const nav = new TableNavigator(table);
+                const newPos = nav.nextCell(editor.selection.start);
+                editor.selection = new vscode.Selection(newPos, newPos);
             }
         }
     }));
 
     ctx.subscriptions.push(vscode.commands.registerCommand('text-tables.gotoPreviousCell', () => {
-        if (!isUndefined(vscode.window.activeTextEditor)) {
-            const editor = vscode.window.activeTextEditor;
-            const tableRange = locator.locate(editor.document, editor.selection.start.line);
-
-            if (tableRange !== undefined) {
-                const tableText = editor.document.getText(tableRange);
-                const table = parser.parse(tableText);
-
-                if (table !== undefined) {
-                    const newText = stringifier.stringify(table);
-                    editor.edit(b => b.replace(tableRange, newText));
-                    const nav = new TableNavigator(table);
-                    const newPos = nav.previousCell(editor.selection.start);
-                    editor.selection = new vscode.Selection(newPos, newPos);
-                }
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const table = formatAndGetTableUnderCursor(editor);
+            if (table) {
+                const nav = new TableNavigator(table);
+                const newPos = nav.previousCell(editor.selection.start);
+                editor.selection = new vscode.Selection(newPos, newPos);
             }
         }
     }));
 
     // Format table under cursor
     ctx.subscriptions.push(vscode.commands.registerCommand('text-tables.formatUnderCursor', () => {
-        if (vscode.window.activeTextEditor !== undefined) {
-            const editor = vscode.window.activeTextEditor;
-            const selectedRange = locator.locate(editor.document, editor.selection.start.line);
-
-            if (selectedRange !== undefined) {
-                const selectedText = editor.document.getText(selectedRange);
-                const table = parser.parse(selectedText);
-
-                if (table !== undefined) {
-                    const newText = stringifier.stringify(table);
-                    editor.edit(b => b.replace(selectedRange, newText));
-                }
-            }
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            formatAndGetTableUnderCursor(editor);
         }
     }));
 }
 
 export function deactivate() {
+}
+
+/**
+ * Try to find table in provided editor in current cursor position.
+ * If table was found, formats the table and returns it. Otherwise - returns undefined.
+ * @param editor active editor
+ * @returns
+ */
+function formatAndGetTableUnderCursor(editor: vscode.TextEditor): Table | undefined {
+    const tableRange = locator.locate(editor.document, editor.selection.start.line);
+
+    if (isUndefined(tableRange)) {
+        return undefined;
+    }
+    const selectedText = editor.document.getText(tableRange);
+    const table = parser.parse(selectedText);
+
+    if (isUndefined(table)) {
+        return undefined;
+    }
+    const newText = stringifier.stringify(table);
+    editor.edit(b => b.replace(tableRange, newText));
+    return table;
 }
