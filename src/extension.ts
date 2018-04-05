8fa8a23a773e091965ee0a457a009297981e5987
Roman Peshkov <peshkovroman@gmail.com>
2018-04-05 14:10:49 +0200

    Added command for table creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { OrgLocator, OrgParser, OrgStringifier  } from './ttOrg';
-import { Locator, Parser, Stringifier, TableNavigator, Table } from './ttTable';
+import { OrgLocator, OrgParser, OrgStringifier } from './ttOrg';
+import { Locator, Parser, Stringifier, TableNavigator, Table, RowType } from './ttTable';
 import { MarkdownLocator, MarkdownParser, MarkdownStringifier } from './ttMarkdown';
 import { isUndefined } from 'util';
 import { registerContext, ContextType, enterContext, exitContext } from './context';
@@ -76,6 +76,53 @@ export function activate(ctx: vscode.ExtensionContext) {
         if (editor) {
             formatAndGetTableUnderCursor(editor);
         }
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand('text-tables.createTable', () => {
+        if (isUndefined(vscode.window.activeTextEditor)) {
+            vscode.window.showInformationMessage('Open editor first');
+            return;
+        }
+
+        const editor = vscode.window.activeTextEditor;
+
+        const re = /^(\d+)x(\d+)$/u;
+
+        const opts: vscode.InputBoxOptions = {
+            value: '5x2',
+            prompt: 'Table size Columns x Rows (e.g. 5x2)',
+            validateInput: (value: string) => {
+                if (!re.test(value)) {
+                    return 'Provided value is invalid. Please provide the value in format Columns x Rows (e.g. 5x2)';
+                }
+                return;
+            }
+        };
+        vscode.window.showInputBox(opts).then(x => {
+            if (!x) {
+                return;
+            }
+
+            const match = x.match(re);
+            if (match) {
+                const cols = +match[1];
+                const rows = +match[2];
+
+                const values = new Array(cols).fill('');
+
+                const table = new Table();
+                for (let i = 0; i < rows; i++) {
+                    table.addRow(RowType.Data, values);
+                }
+
+                const prevSelection = editor.selection;
+                editor.edit(b => {
+                    b.replace(editor.selection, stringifier.stringify(table));
+                    editor.selection = prevSelection;
+                });
+            }
+
+        });
     }));
 }
 
