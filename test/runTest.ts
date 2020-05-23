cec99d158dd1db4b818790c5863d704c7938dc6d
Roman Peshkov <peshkovroman@gmail.com>
2020-05-24 00:33:53 +0200

    Tabulation fix

--- test/runTest.ts
+++ test/runTest.ts
@@ -3,24 +3,24 @@ import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './index');
+        // The path to test runner
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './index');
 
         const testWorkspace = path.resolve(__dirname, '../../e2e')
 
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace]});
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace]});
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
 }
 
 main();
